f87d95d8f2176c360953dc242176e5e39f4e2ecf
Carlos A. Bonilla <lostineverland@gmail.com>
2024-10-10 04:31:04 -0400

    switching to have a separate eager collections tooling

--- src/funcypy/cols.py
+++ src/funcypy/cols.py
@@ -1,69 +1,44 @@
 'Operating on collections'
 
 import functools
-from typing import Callable, Union, Generator
+from typing import Callable, Generator
 from . funcy import complement
 missing = object()
 
 
-def keymap(oper: Callable, obj: dict=missing, lazy=False) -> Union[dict, Generator]:
+def keymap(oper: Callable, obj: dict=missing) -> Generator:
     'Perform a map operation over the keys of a dict'
-    if obj is missing:
-        if lazy == False: return functools.partial(keymap, oper)
-        return functools.partial(keymap, oper, lazy=True)
-    if lazy:
-        for k, v in obj.items():
-            yield oper(k), v
-    else:
-        return dict(keymap(oper, obj, lazy=True))
+    if obj is missing: return functools.partial(keymap, oper)
+    for k, v in obj.items():
+        yield oper(k), v
 
-def valmap(oper: Callable, obj: dict=missing, lazy=False) -> Union[dict, Generator]:
+def valmap(oper: Callable, obj: dict=missing) -> Generator:
     'Perform a map operation over the values of a dict'
-    if obj is missing:
-        if lazy == False: return functools.partial(valmap, oper)
-        return functools.partial(valmap, oper, lazy=True)
-    if lazy:
-        for k, v in obj.items():
-            yield k, oper(v)
-    else:
-        return dict(valmap(oper, obj, lazy=True))
+    if obj is missing: return functools.partial(valmap, oper)
+    for k, v in obj.items():
+        yield k, oper(v)
 
-def itemmap(oper: Callable, obj: dict=missing, lazy=False) -> Union[dict, Generator]:
+def itemmap(oper: Callable, obj: dict=missing) -> Generator:
     'Perform a map operation over the items of a dict'
-    if obj is missing:
-        if lazy == False: return functools.partial(itemmap, oper)
-        return functools.partial(itemmap, oper, lazy=True)
-    if lazy:
-        for k, v in obj.items():
-            yield oper(k, v)
-    else:
-        return dict(itemmap(oper, obj, lazy=True))
+    if obj is missing: return functools.partial(itemmap, oper)
+    for k, v in obj.items():
+        yield oper(k, v)
 
-def keyfilter(oper: Callable, obj: dict=missing, lazy=False) -> Union[dict, Generator]:
-    if obj is missing:
-        if lazy == False: return functools.partial(keyfilter, oper)
-        return functools.partial(keyfilter, oper, lazy=True)
-    if lazy:
-        for k, v in obj.items():
-            if oper(k): yield k, v
-    else:
-        return dict(keyfilter(oper, obj, lazy=True))
+def keyfilter(oper: Callable, obj: dict=missing) -> Generator:
+    if obj is missing: return functools.partial(keyfilter, oper)
+    for k, v in obj.items():
+        if oper(k): yield k, v
 
-def valfilter(oper: Callable, obj: dict=missing, lazy=False) -> Union[dict, Generator]:
-    if obj is missing:
-        if lazy == False: return functools.partial(valfilter, oper)
-        return functools.partial(valfilter, oper, lazy=True)
-    if lazy:
-        for k, v in obj.items():
-            if oper(v): yield k, v
-    else:
-        return dict(valfilter(oper, obj, lazy=True))
+def valfilter(oper: Callable, obj: dict=missing) -> Generator:
+    if obj is missing: return functools.partial(valfilter, oper)
+    for k, v in obj.items():
+        if oper(v): yield k, v
 
-def removekey(oper: Callable, obj: dict=missing, lazy=False) -> Union[dict, Generator]:
-    return keyfilter(complement(oper), obj, lazy)
+def removekey(oper: Callable, obj: dict=missing) -> Generator:
+    return keyfilter(complement(oper), obj)
 
-def removeval(oper: Callable, obj: dict=missing, lazy=False) -> Union[dict, Generator]:
-    return valfilter(complement(oper), obj, lazy)
+def removeval(oper: Callable, obj: dict=missing) -> Generator:
+    return valfilter(complement(oper), obj)
 
 def field_filter(fields, obj=missing):
     'filter for the specified fields from the obj'
@@ -74,19 +49,16 @@ def field_filter(fields, obj=missing):
     )
 
 
-def flatten(obj: dict, _name_space="", lazy=False):
+def flatten(obj: dict, _name_space=""):
     """Takes a nested dict and flattens the values:
         flatten({"some": {"nested": {"value1": 1, "value2": 2}, "other": 3}})
     returns:
         {"some.nested.value1": 1, "some.nested.value2": 2, "some.other": 3}
     """
-    if not lazy: yield dict(flatten(obj, lazy=True))
-    else:
-        for key, val in obj.items():
-            k = ".".join([_name_space, key])
-            if isinstance(val, dict):
-                for kk, vv in flatten(val, k, lazy):
-                    yield kk, vv
-            else:
-                yield k[1:], val
-
+    for key, val in obj.items():
+        k = ".".join([_name_space, key])
+        if isinstance(val, dict):
+            for kk, vv in flatten(val, k, lazy):
+                yield kk, vv
+        else:
+            yield k[1:], val
