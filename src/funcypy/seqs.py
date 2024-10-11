420720bf9be102e21dd809cf6ffbe1090734cfd2
Carlos A. Bonilla <lostineverland@gmail.com>
2024-10-11 18:40:50 -0400

    Add typing info

--- src/funcypy/seqs.py
+++ src/funcypy/seqs.py
@@ -1,9 +1,11 @@
 'tools to handle lazy objects (sequences)'
 
 import functools
+from typing import Generator, Iterable, List, Tuple, Any, Union
+
 missing = object()
 
-def take(n, seq=missing):
+def take(n: int, seq: Iterable=missing) -> List:
     if seq is missing: return functools.partial(take, n)
     vals = []
     for i in range(n):
@@ -13,14 +15,17 @@ def take(n, seq=missing):
             return vals
     return vals
 
-def limit_seq(limit, seq):
+def limit_seq(limit: int, seq: Iterable) -> Generator:
     '''allows you to set a hard limit on a sequence
     '''
     for i, val in enumerate(seq):
         if i == limit: break
         yield val
 
-def loop(val):
+def loop(val: Union[Any, List]) -> Generator:
+    '''Create an infinite loop of val, if val is a list it will
+         be infinitely replayed
+    '''
     if isinstance(val, list):
         while True:
             for i in val:
@@ -28,7 +33,7 @@ def loop(val):
     while True:
         yield val
 
-def nth(n, seq=missing):
+def nth(n: int, seq: Iterable=missing) -> Union[Any, None]:
     if seq is missing: return functools.partial(nth, n)
     N = n - 1
     for i, x in enumerate(seq):
@@ -38,13 +43,13 @@ def nth(n, seq=missing):
     else:
         return x
 
-def last(seq):
+def last(seq: Iterable) -> Union[Any, None]:
     val = None
     for i in seq:
         val = i
     return val
 
-def concat(*seqs):
+def concat(*seqs: Tuple[Iterable]) -> Iterable:
     'concatenate sequences'
     for seq in seqs:
         for i in seq:
