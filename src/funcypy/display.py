33506878d10c29aa08b370487ccbb989594d264d
Carlos A. Bonilla <lostineverland@gmail.com>
2025-07-30 03:58:47 -0400

    fix format lookup

--- src/funcypy/display.py
+++ src/funcypy/display.py
@@ -8,6 +8,12 @@ track = monitor.track(namespace=__name__)
 @partial(count=2)
 @track
 def apply_format(val, fmt, num_fmt='{:,.7g}', str_fmt='{}'):
+    if val is None:
+        try:
+            s = str_fmt.format('-')
+        except:
+            s = '_{}_'.format('-')
+        return s
     if fmt:
         try:
             s = fmt.format(val)
@@ -24,11 +30,12 @@ def table(rows, headers=None, rename={}, fmt={}, num_fmt='{:,.7g}', str_fmt='{}'
     label = lambda k: rename.get(k, k)
     rev_rename = {v: k for k, v in rename.items()}
     rev_label = lambda l: rev_rename.get(l, l)
+    fmt_lookup = lambda k: fmt.get(k, fmt.get(label(k), fmt.get(rev_label(k))))
     if not headers:
         headers = set(k for row in rows for k in row)
     applyFormat = apply_format(num_fmt=num_fmt, str_fmt=str_fmt)
     heading = '|' + '|'.join(applyFormat(label(i), None) for i in headers) + '|'
-    lineFmt = lambda line: '|' + '|'.join(applyFormat(line.get(rev_label(i)), fmt.get(i)) for i in headers) + '|'
+    lineFmt = lambda line: '|' + '|'.join(applyFormat(line.get(rev_label(i)), fmt_lookup(i)) for i in headers) + '|'
     lines = list(lineFmt(row) for row in rows)
     return dict(heading=heading, lines=lines)
 
