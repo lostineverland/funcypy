7f5a2a9db73a78daa88fd4748f14179f84378fd0
Carlos A. Bonilla <lostineverland@gmail.com>
2025-07-30 01:58:55 -0400

    wasn't following the headers

--- src/funcypy/display.py
+++ src/funcypy/display.py
@@ -1,24 +1,34 @@
 'A collection of tools for displaying or pretty printing'
 
 from . funcy import partial
+from . import monitor
 
+track = monitor.track(namespace=__name__)
 
 @partial(count=2)
+@track
 def apply_format(val, fmt, num_fmt='{:,.7g}', str_fmt='{}'):
     if fmt:
-        return fmt.format(val)
+        try:
+            s = fmt.format(val)
+        except:
+            s = '_{}_'.format(val)
+        return s
     elif isinstance(val, str):
         return str_fmt.format(val)
     else:
         return num_fmt.format(val)
 
+@track
 def table(rows, headers=None, rename={}, fmt={}, num_fmt='{:,.7g}', str_fmt='{}'):
     label = lambda k: rename.get(k, k)
+    rev_rename = {v: k for k, v in rename.items()}
+    rev_label = lambda l: rev_rename.get(l, l)
     if not headers:
         headers = set(k for row in rows for k in row)
     applyFormat = apply_format(num_fmt=num_fmt, str_fmt=str_fmt)
     heading = '|' + '|'.join(applyFormat(label(i), None) for i in headers) + '|'
-    lineFmt = lambda line: '|' + '|'.join(applyFormat(v, fmt.get(k)) for k, v in line.items()) + '|'
+    lineFmt = lambda line: '|' + '|'.join(applyFormat(line.get(rev_label(i)), fmt.get(i)) for i in headers) + '|'
     lines = list(lineFmt(row) for row in rows)
     return dict(heading=heading, lines=lines)
 
