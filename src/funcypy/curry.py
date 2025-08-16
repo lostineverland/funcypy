84fe36c884c4ff135234e60f2e887dcb697e442d
Carlos A. Bonilla <lostineverland@gmail.com>
2025-08-15 23:11:15 -0400

    add std lib to curry and raise proper error on wrong attribute

--- src/funcypy/curry.py
+++ src/funcypy/curry.py
@@ -2,6 +2,7 @@
 
 import functools
 import itertools
+import builtins
 from . funcy import partial
 
 
@@ -9,9 +10,19 @@ class Lib:
     """A module wrapper"""
     def __init__(self, lib):
         self.lib = lib
-    def __getattr__(self, arg):
-        return partial(getattr(self.lib, arg))
+    def __getattr__(self, name):
+        try:
+            func = getattr(self.lib, name)
+        except AttributeError:
+            raise AttributeError(f"'{self.__class__.__name__}' object has no attribute '{name}'")
+        # Check if the attribute is callable
+        if callable(func):
+            return functools.update_wrapper(
+                lambda *args, **kwargs: functools.partial(func, *args, **kwargs),
+                func)
+        else:
+            raise AttributeError(f"'{name}' is not a callable attribute in {self.lib.__name__}")
 
 func = Lib(functools)
 iter = Lib(itertools)
-
+stdlib = Lib(builtins)
--- tests/test_curry.py
+++ tests/test_curry.py
@@ -5,6 +5,7 @@ from funcypy import curry
 
 def test_func():
     assert curry.func.reduce(lambda mem, i: mem + i)(range(3)) == 3
+    assert curry.func.reduce(lambda mem, i: mem + i, initial=10)(range(3)) == 13
     assert curry.func.reduce(lambda mem, i: mem + i)(range(3), 10) == 13
 
 def test_iter():
@@ -12,3 +13,6 @@ def test_iter():
     coll = sorted(range(10), key=odd)
     assert {k: list(v) for k,v in curry.iter.groupby(key=odd)(coll)} == {0: [0, 2, 4, 6, 8], 1: [1, 3, 5, 7, 9]}
 
+def test_stdlib():
+    add_1 = lambda x: x + 1
+    assert list(curry.stdlib.map(add_1)(range(3))) == list(range(1, 4))
